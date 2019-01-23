440d06382b0d5cec4cd74f0b3a2ed8541a46cb4a
isabelcooper <isabel@triptease.com>
2019-01-23 09:55:29 +0000

    feature tests for basic functionality

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -10,11 +10,22 @@ feature 'check page loads' do
   end
 end
 
-feature 'if date is today, show happy birthday and name' do
-
+feature 'show happy birthday and name' do
   scenario 'date entered is today' do
     load_and_fill_form(Date.today)
     expect(page).to have_content('Happy Birthday Cat')
   end
+end
+
+feature 'show happy birthday and name' do
+  scenario 'date entered is not today' do
+    load_and_fill_form(Date.today + 10)
+    expect(page).to have_content('10 days until your birthday')
+  end
+
+  # scenario 'date entered if tomorrow' do
+  #   load_and_fill_form(Date.today + 1)
+  #   expect(page).to have_content('1 day until your birthday')
+  # end
 
 end
